f94367ce1a5e99e4ba50a6806999fc2bd2a3b9d9
Abdelnacerr <abdelnacerr@gmail.com>
2021-05-13 16:24:58 +1000

    endpoint done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState } from "react";
+import { fetchQuizQuestions } from "./API";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//components
+import QuestionCard from "./components/QuestionCard";
+
+//types
+import { Difficulty } from "./API";
+const TOTALQ = 10;
+
+const App = () => {
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswers, setUserAnswers] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
+
+	console.log(fetchQuizQuestions(TOTALQ, Difficulty.MEDIUM));
+
+	const startTrivia = async () => {};
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+	const nextQuestion = () => {};
+
+	return (
+		<div className='App'>
+			<h1> REACT QUIZ!</h1>
+			<button className='start' onClick={startTrivia}>
+				Start
+			</button>
+			<p className='score'>Score</p>
+			<p>Loading Questions</p>
+			{/* <QuestionCard
+				questionNr={number + 1}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				callback={checkAnswer}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+				totalQuestions={TOTALQ}
+			/> */}
+			<button className='next' onClick={nextQuestion}>
+				Next
+			</button>
+		</div>
+	);
+};
 
 export default App;
